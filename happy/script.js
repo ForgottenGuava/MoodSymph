dd90a6f705caee5a6ce092e3c20266fb684acafd
ForgottenGuava <115239856+ForgottenGuava@users.noreply.github.com>
2024-04-10 10:37:51 +0530

    Add files via upload

---
+++ happy/script.js
@@ -0,0 +1,7 @@
+var btnLogin = document.getElementById('do-login');
+var idLogin = document.getElementById('login');
+var username = document.getElementById('username');
+btnLogin.onclick = function(){
+window.location.href= "./mood-emoji.html";   
+  // idLogin.innerHTML = '<p style="font-size: 100px">&#129409;</p>' +username.value+ '</h1>';
+}
